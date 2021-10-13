c518b9749b1f4af9fec57d88d504d9a1d8d7bc8b
Cameron White <whitcam20@gmail.com>
2021-10-13 18:18:51 -0400

    moving route code out of server.js

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
 //require modules
 const express = require('express'); //express npm package
-const path = require('path'); // Node.js native utility module
 
 //set express as variable to use easily later in code
 const app = express();
 //set up server on port 3001
-const PORT = process.env.port || 3001;
+const PORT = process.env.port || 3002;
 
-//import routers and set to variables
-const routes = require('./routes/apiRoutes');
-const routes2 = require('./routes/htmlRoutes');
+// //import routers and set to variables
+// const routes = require('./routes/apiRoutes');
+// const routes2 = require('./routes/htmlRoutes');
 
 //Middleware used for parsing JSON data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));  //static file in public folder
 
-//use router
-app.use('/api', routes);
-app.use('/', routes2);
+// //use router
+// app.use('/api', routes);
+// app.use('/', routes2);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-//route to GET home(index)
-app.get('/', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-//route to GET notes
-app.get('/notes', (req,res) => 
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+// //route to GET home(index)
+// app.get('/', (req, res) => 
+//     res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
+// //route to GET notes
+// app.get('/notes', (req,res) => 
+//     res.sendFile(path.join(__dirname, '/public/notes.html'))
+// );
 
 //open app to listen on port
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
+   console.log(`listening at http://localhost:${PORT}`)
+ );
+
