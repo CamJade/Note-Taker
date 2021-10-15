2842e05370cfbdaf70523812171f1c86ba5ae291
Cameron White <whitcam20@gmail.com>
2021-10-14 23:03:21 -0400

    update comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,8 @@
-// //bring in express
-// const express = require('express');
+// //require modules
+// const express = require('express'); //express npm package
+
+// //set express as variable to use easily later in code
+// const app = express();
 const path = require('path');
 
 
@@ -8,15 +11,12 @@ const path = require('path');
 //create module.exports so other files can see
 module.exports = (app) => {
 
-// `GET /notes` should return the `notes.html` file.
+    // `GET /notes` should return the `notes.html` file.
     app.get("/notes", (req, res) => 
         res.sendFile(path.join(__dirname, "../public/notes.html")));
 
-//`GET *` should return the `index.html` file.
+    //`GET *` should return the `index.html` file.
     app.get("*", (req, res) => 
         res.sendFile(path.join(__dirname, "../public/index.html")));
 
-    //app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
-    
-    
 };
